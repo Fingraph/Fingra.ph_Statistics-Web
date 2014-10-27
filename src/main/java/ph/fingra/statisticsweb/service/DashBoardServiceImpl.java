2c0abded49f8074d8a3d2b8e7224e21b9ab4ac68
Kim Tae-woo <kim@tgrape.com>
2014-10-27 10:29:16 +0900

    fix a DashBoardService Implementation. (add @Service annotation)

--- src/main/java/ph/fingra/statisticsweb/service/DashBoardServiceImpl.java
+++ src/main/java/ph/fingra/statisticsweb/service/DashBoardServiceImpl.java
@@ -27,6 +27,7 @@ import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.google.gson.JsonArray;
@@ -46,6 +47,7 @@ import ph.fingra.statisticsweb.domain.DashBoard;
 import ph.fingra.statisticsweb.domain.DashBoardSearchParam;
 import ph.fingra.statisticsweb.exception.UnauthorizedAccessException;
 
+@Service
 public class DashBoardServiceImpl implements DashBoardService {
 
 	@Autowired
